61fd85ecb1d4382e54e96a3a4d81931b25154f8f
joaquinalvidrez95 <joaquin_alan@hotmail.com>
2018-05-28 18:05:20 -0500

    Initial commit

---
+++ app/src/main/java/com/citsadigital/pantallamultideportiva/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.citsadigital.pantallamultideportiva
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
